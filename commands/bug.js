2164878c9e15efb3076d4412859a7af9fee50219
Fabien Rahaingo <fabrahaingo@hotmail.fr>
2022-09-14 16:12:07 +0200

    used sendMessage instead of sendLongText

--- commands/bug.js
+++ commands/bug.js
@@ -1,11 +1,11 @@
-const { sendLongText } = require("../utils/handleLongText")
+// const { sendLongText } = require("../utils/handleLongText")
 
 module.exports = bot => async msg => {
     const chatId = msg.chat.id
     try {
         await bot.sendChatAction(chatId, "typing")
         const text = `Un problème ? Merci de contacter hellofabien@pm.me en mentionnant votre identifiant Telegram (*${msg.from.id}*)`
-        await sendLongText(bot, chatId, text)
+        await bot.sendMessage(chatId, text)
     } catch (error) {
         console.log(error)
     }
