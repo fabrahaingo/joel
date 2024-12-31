7d17ed7ddf2dadae63af0d2e6f923c4ac3dec5fc
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2024-12-31 12:49:26 +0100

    Added check for MONGODB_URI env variable

--- db.ts
+++ db.ts
@@ -2,6 +2,12 @@ import mongoose from "mongoose";
 import { ErrorMessages } from "./entities/ErrorMessages";
 
 export const mongodbConnect = async () => {
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  if (MONGODB_URI === undefined) {
+    throw new Error(ErrorMessages.MONGODB_URI_NOT_SET);
+  }
+
   if (!process.env.MONGODB_URI) {
     throw new Error(ErrorMessages.MONGODB_URI_NOT_SET);
   }
