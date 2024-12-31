80913675187ab0e075ec82f0ba480adf3c6bc3cb
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2024-12-31 12:53:37 +0100

    Added dateTOJORFFormat

--- utils/date.utils.ts
+++ utils/date.utils.ts
@@ -7,3 +7,21 @@ export function dateToFrenchString(date: string): string {
     day: "numeric",
   });
 }
+
+export function dateTOJORFFormat(date: Date): string {
+  const dateISO = date.toISOString().split(/[-T]/);
+  return `${dateISO[2]}-${dateISO[1]}-${dateISO[0]}`;
+}
+
+export function JORFtoDate(dateStr: string): Date {
+  const dateSplit = dateStr.split("-");
+
+  const date = new Date(
+    parseInt(dateSplit[0]),
+    parseInt(dateSplit[1]),
+    parseInt(dateSplit[2]),
+  );
+  date.setHours(0, 0, 0, 0);
+
+  return date;
+}
