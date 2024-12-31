036d17366ab8ad096dfe19a36c4326bc2fabfdb9
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2024-12-31 12:59:42 +0100

    More comprehensive and backlog processing of user notifications

--- scripts/notifyUsers.ts
+++ scripts/notifyUsers.ts
@@ -1,329 +1,74 @@
-require("dotenv").config();
-import { ChatId } from "node-telegram-bot-api";
-const mongoose = require("mongoose");
-import People from "../models/People";
-import User from "../models/User";
-import Blocked from "../models/Blocked";
-import axios from "axios";
-const { formatSearchResult } = require("../utils/formatSearchResult");
-import { splitText } from "../utils/sendLongText";
-import { Types } from "mongoose";
-const umami = require("../utils/umami");
-const { ChatId } = require("node-telegram-bot-api");
+import "dotenv/config";
 
-type IUser = {
-  chatId: number;
-  _id: number;
-  followedPeople: {
-    peopleId: string;
-    lastUpdate: Date;
-  }[];
-  followedFunctions: string[];
-  save: () => any;
-};
+import { mongodbConnect } from "../db";
+import { ErrorMessages } from "../entities/ErrorMessages";
+import {
+  forceNotifyTagUpdates,
+  forceNotifyTagUpdatesFromDate,
+  getJORFRecordsFromDate,
+  notifyPeopleUpdates,
+  updatePeopleFromTags,
+  updatePeopleInDB,
+} from "../utils/update.utils";
 
-async function filterOutBlockedUsers(users: any[]): Promise<IUser[]> {
-  const blockedUsers = await Blocked.find({}, { chatId: 1 });
-  for (let blockedUser of blockedUsers) {
-    users = users.filter(
-      (user) => user.chatId.toString() !== blockedUser.chatId.toString()
-    );
-  }
-  return users;
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+if (BOT_TOKEN === undefined) {
+  throw new Error("BOT TOKEN NOT SET");
 }
 
-// only retrieve people who have been updated on same day
-async function getPeople() {
-  // get date in format YYYY-MM-DD
-  const currentDate = new Date().toISOString().split("T")[0];
-  // const currentDate = "2024-02-18";
-  const people = await People.find(
-    {
-      updatedAt: {
-        $gte: new Date(currentDate),
-      },
-    },
-    { _id: 1, lastKnownPosition: 1, updatedAt: 1 }
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (MONGODB_URI === undefined) {
+  throw new Error(ErrorMessages.MONGODB_URI_NOT_SET);
+}
+
+// Force :
+// - Update people from JORF
+// - Send notifications when lastUpdate does not match
+// - Send tag notifications for the past period
+
+// Require to load all users, all people, compare update and notification time
+
+(async () => {
+  // Number of months to go back
+  const shiftDays = 5;
+
+  // currentDate is today
+  const currentDate = new Date();
+  const startDate = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    currentDate.getDate() - shiftDays,
   );
-  return people;
-}
 
-// the argument is a list of _id of people who have been updated
-// retrieve all users who follow at least one of these people
-async function getUsers(updatedPeople: string[]) {
-  const peopleIdStringArray = returnIdsArray(updatedPeople).map((id: any) =>
-    id.toString()
-  );
-  const currentDate = new Date().toISOString().split("T")[0];
-  const users: IUser[] = await User.find(
-    {
-      $or: [
-        {
-          followedPeople: {
-            $elemMatch: {
-              peopleId: {
-                $in: peopleIdStringArray,
-              },
-            },
-          },
-        },
-        {
-          followedFunctions: {
-            $ne: [],
-          },
-        },
-      ],
-    },
-    { _id: 1, followedPeople: 1, followedFunctions: 1, chatId: 1 }
-  ).then(async (res: any[]) => {
-    return await filterOutBlockedUsers(res);
-  });
+  startDate.setHours(0, 0, 0, 0);
+  const JORFUpdate = await getJORFRecordsFromDate(startDate);
 
-  for (let user of users) {
-    let followed: {
-      peopleId: string;
-      lastUpdate: Date;
-    }[] = [];
-    for (let followedPerson of user.followedPeople) {
-      const idUpdated = peopleIdStringArray.includes(
-        followedPerson.peopleId.toString()
-      );
-      const lastUpdate = new Date(followedPerson.lastUpdate)
-        .toISOString()
-        .split("T")[0];
-      if (idUpdated && lastUpdate !== currentDate) {
-        followed.push(followedPerson);
-      }
-    }
-    user.followedPeople = followed;
-  }
-  return users;
-}
+  // Connect to DB
+  await mongodbConnect();
 
-async function sendUpdate(user: IUser, peopleUpdated: string | any[]) {
-  if (!user.chatId) {
-    return;
+  // Update people and return all people records to be sent to users
+  const updatedPeopleRecords = await updatePeopleInDB(JORFUpdate);
+
+  // Update and return tags
+  const updatedTagMap = await updatePeopleFromTags(JORFUpdate);
+
+  // Notify users
+  const BOT_TOKEN = process.env.BOT_TOKEN;
+
+  if (BOT_TOKEN === undefined) {
+    throw new Error("BOT TOKEN NOT SET");
   }
 
-  const tagsList = user.followedFunctions;
-  let peopleFromFunctions:
-    | {
-        [x: string]: any;
-      }
-    | any = {};
-  if (tagsList) {
-    for (let tag of tagsList) {
-      let listOfPeopleFromTag = await People.find(
-        {
-          [`lastKnownPosition.${tag}`]: {
-            $exists: true,
-          },
-          updatedAt: {
-            $gte: new Date(new Date().setHours(0, 0, 0, 0)),
-          },
-        },
-        { _id: 1, lastKnownPosition: 1, updatedAt: 1 }
-      );
-      if (listOfPeopleFromTag.length > 0) {
-        peopleFromFunctions[tag] = listOfPeopleFromTag;
-      }
-    }
-  }
+  // False (or missing): only notify users which records show that they have not been notified of these updates
+  // True: notify all users following of all updates they follow in updatedPeopleRecords
+  const FORCE_NOTIFY = false;
 
-  if (Object.keys(peopleFromFunctions).length > 0 || peopleUpdated.length > 0) {
-    let notification_text =
-      "📢 Aujourd'hui, il y a eu de nouvelles publications pour les personnes que vous suivez !\n\n";
+  await notifyPeopleUpdates(updatedPeopleRecords, BOT_TOKEN, FORCE_NOTIFY);
 
-    for (let person of peopleUpdated) {
-      let prenomNom = `${person.lastKnownPosition.prenom} ${person.lastKnownPosition.nom}`;
-      let prenomNomLink = `[${prenomNom}](https://jorfsearch.steinertriples.ch/name/${encodeURI(
-          prenomNom)})`;
-      notification_text += `Nouvelle publication pour ${prenomNomLink}\n`;
-      notification_text += formatSearchResult([person.lastKnownPosition], {
-        isListing: true,
-      });
-      if (peopleUpdated.indexOf(person) + 1 !== peopleUpdated.length)
-        notification_text += "\n";
-    }
+  // Force notify by default, as the last notification time is not saved
+  await forceNotifyTagUpdates(updatedTagMap, BOT_TOKEN);
 
-    for (let tag in peopleFromFunctions) {
-      notification_text += "====================\n\n";
-      notification_text += `Nouvelle publication pour les personnes suivies avec le tag *${tag}*:\n\n`;
-      for (let person of peopleFromFunctions[tag]) {
-        let prenomNom = `${person.lastKnownPosition.prenom} ${person.lastKnownPosition.nom}`;
-        let prenomNomLink = `[${prenomNom}](https://jorfsearch.steinertriples.ch/name/${encodeURI(
-            prenomNom)})`;
-        notification_text += `${prenomNomLink}\n`;
-        notification_text += formatSearchResult([person.lastKnownPosition], {
-          isListing: true,
-        });
-        if (
-          peopleFromFunctions[tag].indexOf(person) + 1 ===
-          peopleFromFunctions[tag].length
-        )
-          notification_text += "\n";
-      }
-      if (
-        Object.keys(peopleFromFunctions).indexOf(tag) + 1 !==
-        Object.keys(peopleFromFunctions).length
-      )
-        notification_text += "\n";
-    }
-
-    const messagesArray = splitText(notification_text, 3000);
-
-    let blocked = false;
-    let alreadyNotified: ChatId[] = [];
-    for await (let message of messagesArray) {
-      if (blocked) return;
-      if (alreadyNotified.includes(user.chatId)) {
-        return;
-      }
-      await axios
-        .post(
-          `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-          {
-            chat_id: user.chatId,
-            text: message,
-            parse_mode: "markdown",
-            link_preview_options: {
-              is_disabled: true,
-            },
-          }
-        )
-        .catch(async (err) => {
-          if (
-            err.response.data.description ===
-            "Forbidden: bot was blocked by the user"
-          ) {
-            await umami.log({ event: "/user-blocked-joel" });
-            await new Blocked({
-              chatId: user.chatId,
-            }).save();
-            blocked = true;
-            return;
-          }
-          console.log(err.message);
-        });
-    }
-
-    await umami.log({ event: "/notification-update" });
-  }
-}
-
-async function populatePeople(user: IUser, peoples: any[]) {
-  const peopleUpdated: {
-    _id: any;
-    lastKnownPosition: {
-      prenom: string;
-      nom: string;
-    };
-  }[] = [];
-  for await (let followedPerson of user.followedPeople) {
-    const person = peoples.find(
-      (person) => person._id.toString() === followedPerson.peopleId.toString()
-    );
-    if (person) {
-      peopleUpdated.push(person);
-    }
-  }
-  return peopleUpdated;
-}
-
-async function updateUser(
-  user: IUser,
-  peoples: {
-    _id: Types.ObjectId;
-    nom: string;
-    prenom: string;
-    lastKnownPosition: any[];
-  }[]
-) {
-  const peoplesIdArray: string[] = returnIdsArray(peoples).map((id) =>
-    (id as ChatId).toString()
-  );
-  const userFromDb: IUser = (await User.findById(user._id)) as IUser;
-
-  for (let followedPerson of userFromDb.followedPeople) {
-    if (peoplesIdArray.includes(followedPerson.peopleId.toString())) {
-      followedPerson.lastUpdate = new Date();
-    }
-  }
-  // remove duplicated in followedPeople array that have same peopleId (can happen if user has followed a person twice)
-  userFromDb.followedPeople = userFromDb.followedPeople.filter(
-    (
-      followedPerson: { peopleId: { toString: () => any } },
-      index: any,
-      self: any[]
-    ) =>
-      index ===
-      self.findIndex(
-        (t) => t.peopleId.toString() === followedPerson.peopleId.toString()
-      )
-  );
-  // save user
-  await userFromDb.save();
-}
-
-async function notifyUsers(
-  users: IUser[],
-  peoples: {
-    _id: Types.ObjectId;
-    nom: string;
-    prenom: string;
-    lastKnownPosition: any;
-  }[]
-) {
-  for await (let user of users) {
-    // create an array of people who have been updated
-    let peopleUpdated = await populatePeople(user, peoples);
-    if (peopleUpdated.length || user.followedFunctions.length) {
-      // remove duplicates from peopleUpdated array
-      peopleUpdated = peopleUpdated.filter(
-        (person, index, self) =>
-          index ===
-          self.findIndex((t) => t._id.toString() === person._id.toString())
-      );
-      // update field updatedAt in followedPeople
-      await updateUser(user, peoples);
-      // send notification to user
-      await sendUpdate(user, peopleUpdated);
-    }
-    // prevent hitting Telegram API rate limit
-    await new Promise((resolve) => setTimeout(resolve, 500));
-  }
-}
-
-function returnIdsArray(
-  arr:
-    | {
-        _id: any;
-      }[]
-    | any[]
-) {
-  let res: string[] = [];
-  for (let item of arr) {
-    res.push(item._id);
-  }
-  return res;
-}
-
-mongoose
-  .connect(process.env.MONGODB_URI || "")
-  .then(async () => {
-    // 1. get all people who have been updated today
-    const peoples = await getPeople();
-    if (peoples.length === 0) {
-      process.exit(0);
-    }
-    const peopleIds = returnIdsArray(peoples);
-    // 2. get all users who follow at least one of these people
-    const users = await getUsers(peopleIds);
-    // 3. send notification to users
-    await notifyUsers(users, peoples);
-    process.exit(0);
-  })
-  .catch((err: any) => {
-    console.log(err);
-    process.exit(1);
-  });
+  process.exit(0);
+})();
---
+++ utils/update.utils.ts
@@ -0,0 +1,432 @@
+import {
+  JORFSearchItem,
+  JORFSearchResponse,
+} from "../entities/JORFSearchResponse";
+import { FunctionTags } from "../entities/FunctionTags";
+import { IPeople, IUser } from "../types";
+import People from "../models/People";
+import umami from "./umami";
+import { dateTOJORFFormat, JORFtoDate } from "./date.utils";
+import axios, { AxiosError } from "axios";
+import Blocked from "../models/Blocked";
+import User from "../models/User";
+import { formatSearchResult } from "./formatSearchResult";
+import { splitText } from "./sendLongText";
+import { ChatId } from "node-telegram-bot-api";
+import { Types } from "mongoose";
+
+function isItemTagged(item: JORFSearchItem, tag: FunctionTags) {
+  return Object.prototype.hasOwnProperty.call(item, tag);
+}
+
+function extractTaggedItems(JORF_items: JORFSearchItem[], tag: FunctionTags) {
+  return JORF_items.filter((item) => isItemTagged(item, tag));
+}
+
+export async function updatePeopleFromTags(updatedRecords: JORFSearchItem[]) {
+  // Order records by date: latest on top
+
+  // extracts the relevant tags from the daily updates
+  // format: {tag: [contacts], tag2: [contacts]}
+  const itemTagMap: [[FunctionTags], JORFSearchItem[]][] = [];
+
+  let peopleList: JORFSearchItem[] = [];
+
+  for (const tag of Object.values(FunctionTags)) {
+    // Add records corresponding to each tag extraction
+    const tagExtraction = extractTaggedItems(updatedRecords, tag);
+    if (tagExtraction.length > 0) {
+      itemTagMap[tag] = tagExtraction;
+      peopleList = peopleList.concat(tagExtraction);
+    }
+  }
+
+  // Remove duplicates in the list to limit db queries
+  const reducedPeopleList = peopleList.filter(
+    (people, index, self) =>
+      index ==
+      self.findIndex((t) => t.nom == people.nom && t.prenom == people.prenom),
+  );
+
+  for (const record of reducedPeopleList) {
+    const people: IPeople | null = await People.findOne({
+      nom: record.nom,
+      prenom: record.prenom,
+    });
+
+    // If People in dB, update the lastKnownPosition
+    if (people !== null) await updatePeople([{ people, records: [record] }]);
+    // if the person doesn't exist, create a new one
+    else {
+      const newPerson: IPeople = new People({
+        nom: record.nom,
+        prenom: record.prenom,
+        lastKnownPosition: record,
+      });
+      await newPerson.save();
+      await umami.log({ event: "/person-added" });
+    }
+  }
+  return itemTagMap;
+}
+
+export async function getJORFRecordsFromDate(
+  startDate: Date,
+): Promise<JORFSearchItem[]> {
+  const todayDate = new Date();
+
+  // In place operations
+  startDate.setHours(0, 0, 0, 0);
+  todayDate.setHours(0, 0, 0, 0);
+
+  const startDateStr = dateTOJORFFormat(startDate);
+
+  // From today, until the start
+  // Order is important to keep record sorted, and remove later ones as duplicates
+  let updatedPeople: JORFSearchItem[] = [];
+  for (
+    const currentDate = new Date(todayDate);
+    dateTOJORFFormat(currentDate) !== startDateStr;
+    currentDate.setDate(currentDate.getDate() - 1)
+  ) {
+    const JORFPeople: JORFSearchItem[] = await getDailyUpdate(currentDate);
+
+    updatedPeople = updatedPeople.concat(JORFPeople);
+  }
+  return updatedPeople;
+}
+
+async function getRelevantPeopleFromDb(
+  recordsList: JORFSearchItem[],
+): Promise<{ people: IPeople; records: JORFSearchItem[] }[]> {
+  if (recordsList.length === 0) return [];
+
+  const peopleList: IPeople[] = await People.find({
+    $or: recordsList.map((record) => ({
+      nom: record.nom,
+      prenom: record.prenom,
+    })),
+  });
+
+  const relevantPeopleMap: { people: IPeople; records: JORFSearchItem[] }[] =
+    [];
+
+  for (const people of peopleList) {
+    relevantPeopleMap.push({
+      people,
+      records: recordsList.filter(
+        (t) => t.nom == people.nom && t.prenom == people.prenom,
+      ),
+    });
+  }
+  return relevantPeopleMap;
+}
+
+async function updatePeople(
+  peopleUpdates: { people: IPeople; records: JORFSearchItem[] }[],
+) {
+  for (const peopleUpdate of peopleUpdates) {
+    const people = peopleUpdate.people;
+
+    for (const record of peopleUpdate.records) {
+      const source_people_id = parseInt(
+        people.lastKnownPosition.source_id.split("JORFTEXT").slice(-1)[0],
+      );
+      const source_record_id = parseInt(
+        record.source_id.split("JORFTEXT").slice(-1)[0],
+      );
+
+      // Record is older (day) than last registered
+      if (source_people_id >= source_record_id) {
+        continue;
+      }
+
+      people.lastKnownPosition = record;
+      await umami.log({ event: "/person-updated" });
+      await people.save();
+    }
+  }
+}
+
+export async function getDailyUpdate(date: Date): Promise<JORFSearchItem[]> {
+  const todayDate = new Date();
+  todayDate.setHours(0, 0, 0, 0);
+
+  if (date.getTime() > todayDate.getTime()) {
+    throw Error("Unable to fetch JORF updates in future dates");
+  }
+  const dailyUpdates = await axios
+    .get<JORFSearchResponse>(
+      `https://jorfsearch.steinertriples.ch/${dateTOJORFFormat(date)}?format=JSON`,
+    )
+    .then((res) => res.data)
+    .catch((err: unknown) => {
+      console.log(err);
+      return [];
+    });
+
+  if (dailyUpdates === null || typeof dailyUpdates === "string") {
+    return [];
+  }
+
+  return dailyUpdates;
+  // Do not filter out duplicates: only the first item of each people will be keep for record keeping;
+}
+
+// Update people in DB from starting date and return JORFItems of updated
+export async function updatePeopleInDB(updatedRecords: JORFSearchItem[]) {
+  const relevantPeople = await getRelevantPeopleFromDb(updatedRecords);
+  await updatePeople(relevantPeople);
+
+  return relevantPeople;
+}
+
+async function filterOutBlockedUsers(users: IUser[]): Promise<IUser[]> {
+  const blockedUsers: IUser[] = await Blocked.find({}, { _id: 1 });
+  for (const blockedUser of blockedUsers) {
+    users = users.filter((user) => user._id === blockedUser._id);
+  }
+  return users;
+}
+
+async function updateUserFollows(user: IUser, peoples: IPeople[]) {
+  const peoplesIdArray = peoples.map((people) => people._id);
+
+  for (const followedPerson of user.followedPeople) {
+    if (peoplesIdArray.includes(followedPerson.peopleId)) {
+      followedPerson.lastUpdate = new Date();
+    }
+  }
+  // remove duplicated in followedPeople array that have same peopleId (can happen if user has followed a person twice)
+  user.followedPeople = user.followedPeople.filter(
+    (followedPerson: { peopleId: Types.ObjectId }, index: number, self) =>
+      index ===
+      self.findIndex(
+        (t: { peopleId: Types.ObjectId }) =>
+          t.peopleId === followedPerson.peopleId,
+      ),
+  );
+  // save user
+  await user.save();
+}
+
+// There is currently no way to check if a user has been notified of a tag update
+// Resuming an update thus require to force-notify users for all tags update over the period.
+// Before: use updateTagsFromDate to update db
+export async function forceNotifyTagUpdates(
+  tagMap: [[FunctionTags], JORFSearchItem[]][],
+  BOT_TOKEN: string,
+) {
+  const tagListStr = Object.keys(tagMap);
+
+  const updatedUsers: IUser[] = await User.find({
+    followedFunctions: {
+      $elemMatch: {
+        $in: tagListStr,
+      },
+    },
+  }).then(async (res: IUser[]) => {
+    return await filterOutBlockedUsers(res);
+  });
+
+  for (const user of updatedUsers) {
+    // send notification to user
+    await sendForcedTagUpdates(user, tagMap, BOT_TOKEN);
+  }
+}
+
+export async function notifyPeopleUpdates(
+  updatedPeopleRecords: { people: IPeople; records: JORFSearchItem[] }[],
+  BOT_TOKEN: string,
+  FORCE_NOTIFY?: boolean,
+) {
+  // Search all users at once to reduce strain on DB
+  const peopleIdStringArray = updatedPeopleRecords.map((i) =>
+    i.people._id.toString(),
+  );
+
+  const updatedUsers: IUser[] = await User.find({
+    followedPeople: {
+      $elemMatch: {
+        peopleId: {
+          $in: peopleIdStringArray,
+        },
+      },
+    },
+  }).then(async (res: IUser[]) => {
+    return await filterOutBlockedUsers(res);
+  });
+
+  for (const user of updatedUsers) {
+    const userFollowedIdsStr = user.followedPeople.map((item) =>
+      item.peopleId.toString(),
+    );
+
+    // Filter people followed by the user
+    const userSpecificUpdates = updatedPeopleRecords.filter(
+      (item) =>
+        userFollowedIdsStr.findIndex((t) => item.people._id.toString() === t) >=
+        0,
+    );
+    // send notification to user
+    await sendPeopleUpdate(user, userSpecificUpdates, BOT_TOKEN, FORCE_NOTIFY);
+
+    await updateUserFollows(
+      user,
+      userSpecificUpdates.map((t) => t.people),
+    );
+  }
+}
+
+async function sendPeopleUpdate(
+  user: IUser,
+  peopleUpdated: { people: IPeople; records: JORFSearchItem[] }[],
+  BOT_TOKEN: string,
+  FORCE_NOTIFY?: boolean,
+) {
+  // Drop records of people that are up to date
+  let filteredUpdates: { people: IPeople; records: JORFSearchItem[] }[] = [];
+
+  if (FORCE_NOTIFY) {
+    filteredUpdates = peopleUpdated;
+  } else {
+    for (const peopleUpdate of peopleUpdated) {
+      // Filter records that are up to date
+      const idPeople = user.followedPeople.findIndex(
+        (p) => p.peopleId.toString() === peopleUpdate.people._id.toString(),
+      );
+      if (idPeople < 0) {
+        continue;
+      }
+      const lastUpdateDate = user.followedPeople[idPeople].lastUpdate;
+      const relevantRecords = peopleUpdate.records.filter(
+        (r) => JORFtoDate(r.source_date).getTime() > lastUpdateDate.getTime(),
+      );
+
+      // If records are left, we had the people to the notification pile
+      if (relevantRecords.length > 0) {
+        filteredUpdates.push({
+          people: peopleUpdate.people,
+          records: relevantRecords,
+        });
+      }
+    }
+  }
+
+  if (filteredUpdates.length == 0) {
+    return;
+  }
+  let notification_text = "📢 ";
+
+  if (filteredUpdates.length > 1) {
+    notification_text +=
+      "Nouvelles publications pour les personnes que vous suivez :\n\n";
+  }
+
+  for (const peopleUpdate of filteredUpdates) {
+    const prenomNom = `${peopleUpdate.people.prenom} ${peopleUpdate.people.nom}`;
+    const prenomNomLink = `[${prenomNom}](https://jorfsearch.steinertriples.ch/name/${encodeURI(
+      prenomNom,
+    )})`;
+
+    const pluralHandler = peopleUpdate.records.length > 1 ? "s" : "";
+
+    notification_text += `Nouvelle${pluralHandler} publication${pluralHandler} pour ${prenomNomLink}\n`;
+    notification_text += formatSearchResult(peopleUpdate.records, {
+      isListing: true,
+    });
+
+    if (filteredUpdates.indexOf(peopleUpdate) + 1 !== filteredUpdates.length)
+      notification_text += "====================\n\n";
+
+    notification_text += "\n";
+  }
+
+  await sendLongMessageFromAxios(user, notification_text, BOT_TOKEN);
+
+  await umami.log({ event: "/notification-update-people" });
+}
+
+async function sendForcedTagUpdates(
+  user: IUser,
+  tagMap: [[FunctionTags], JORFSearchItem[]][],
+  BOT_TOKEN: string,
+) {
+  if (tagMap.length == 0) {
+    return;
+  }
+  let notification_text = "📢 ";
+
+  if (tagMap.length > 1) {
+    notification_text +=
+      "Nouvelles publications pour les fonctions que suivez :\n\n";
+  }
+
+  const tagList = Object.keys(tagMap) as FunctionTags[];
+
+  for (const tag of tagList) {
+    const records: JORFSearchItem[] = tagMap[tag];
+    const pluralHandler = records.length > 1 ? "s" : "";
+
+    notification_text += `Nouvelle${pluralHandler} publication${pluralHandler} pour *${tag}*\n`;
+
+    for (const record of records) {
+      const prenomNom = `${record.prenom} ${record.nom}`;
+      const prenomNomLink = `[${prenomNom}](https://jorfsearch.steinertriples.ch/name/${encodeURI(
+        prenomNom,
+      )})`;
+      notification_text += `${prenomNomLink}\n`;
+      notification_text += formatSearchResult([record], {
+        isListing: true,
+      });
+      if (records.indexOf(record) + 1 !== records.length)
+        notification_text += "\n";
+    }
+
+    if (tagList.indexOf(tag) + 1 !== tagList.length)
+      notification_text += "====================\n\n";
+
+    notification_text += "\n";
+  }
+
+  await sendLongMessageFromAxios(user, notification_text, BOT_TOKEN);
+
+  await umami.log({ event: "/notification-update-tag" });
+}
+
+async function sendLongMessageFromAxios(
+  user: IUser,
+  message: string,
+  BOT_TOKEN: string,
+) {
+  const messagesArray = splitText(message, 3000);
+
+  for (const message of messagesArray) {
+    await axios
+      .post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+        chat_id: user.chatId as ChatId,
+        text: message,
+        parse_mode: "markdown",
+        link_preview_options: {
+          is_disabled: true,
+        },
+      })
+      .catch(async (err: unknown) => {
+        const error = err as AxiosError;
+        if (
+          (error.response.data.description as string) ===
+          "Forbidden: bot was blocked by the user"
+        ) {
+          await umami.log({ event: "/user-blocked-joel" });
+          await new Blocked({
+            chatId: user.chatId as ChatId,
+          }).save();
+          return;
+        }
+        console.log(error.message);
+      });
+
+    // prevent hitting Telegram API rate limit
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+}
