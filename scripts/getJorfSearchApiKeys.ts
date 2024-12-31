86c77325ed840b54abbc264c34b29c9e446d57d8
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2024-12-31 12:54:45 +0100

    Strong typing of getJorfSearchApiKeys.ts

--- scripts/getJorfSearchApiKeys.ts
+++ scripts/getJorfSearchApiKeys.ts
@@ -1,19 +1,29 @@
-import axios from "axios";
-import moment from "moment";
+import axios, { AxiosResponse } from "axios";
+import fs from "fs";
+import {
+  JORFSearchItem,
+  JORFSearchResponse,
+} from "../entities/JORFSearchResponse";
+import { dateTOJORFFormat } from "../utils/date.utils";
 
-async function JorfSearchCall(currentDay: string) {
+async function JORFSearchCall(currentDay: string): Promise<JORFSearchItem[]> {
   return await axios
-    .get(`https://jorfsearch.steinertriples.ch/${currentDay}?format=JSON`)
-    .then((res) => {
+    .get<JORFSearchResponse>(
+      `https://jorfsearch.steinertriples.ch/${currentDay}?format=JSON`,
+    )
+    .then((res: AxiosResponse<JORFSearchResponse>) => {
+      if (res.data === null || typeof res.data == "string") {
+        return [];
+      }
       return res.data;
     });
 }
 
 // We need to go through the data and get all the possible keys. The goal is to build a type of the data
-function getAllPossibleKeys(res: any) {
-  let keys: string[] = [];
-  for (let contact of res) {
-    for (let key of Object.keys(contact)) {
+function getAllPossibleKeys(res: JORFSearchItem[]) {
+  const keys: string[] = [];
+  for (const contact of res) {
+    for (const key of Object.keys(contact)) {
       if (!keys.includes(key)) {
         keys.push(key);
       }
@@ -25,13 +35,13 @@ function getAllPossibleKeys(res: any) {
 async function main() {
   const allPossibleKeys = new Set<string>();
 
-  let currentDay = "02-17-2024";
+  const currentDay = new Date();
   for (let i = 0; i < 1500; i++) {
     // currentDay minus i days
     const day = new Date(currentDay);
     day.setDate(day.getDate() - i);
-    const formattedDate = moment(day).format("DD-MM-YYYY");
-    const res = await JorfSearchCall(formattedDate);
+
+    const res = await JORFSearchCall(dateTOJORFFormat(day));
 
     const keys = getAllPossibleKeys(res);
     keys.forEach((key) => allPossibleKeys.add(key));
@@ -40,22 +50,21 @@ async function main() {
       // remove 2 lines from console
       process.stdout.moveCursor(0, -2);
     }
-    console.log(`Day ${i} done. ${1500 - i} days left.`);
-    console.log(`Found ${allPossibleKeys.size} keys.`);
+    console.log(`Day ${String(i)} done. ${String(1500 - i)} days left.`);
+    console.log(`Found ${String(allPossibleKeys.size)} keys.`);
     await new Promise((resolve) => setTimeout(resolve, 100));
 
     // every 100 days, write the keys to a file
     if (i % 100 === 0) {
-      const fs = require("fs");
       fs.writeFileSync(
         "allPossibleKeys.txt",
-        Array.from(allPossibleKeys).join("\n")
+        Array.from(allPossibleKeys).join("\n"),
       );
     }
   }
 
   console.log("All possible keys:");
-  for (let key of allPossibleKeys) {
+  for (const key of allPossibleKeys) {
     console.log(key);
   }
 }
