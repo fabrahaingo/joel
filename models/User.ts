17a49c4e13263226da4f428f18ab13e3adf116c5
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2025-03-24 21:41:21 +0100

    set required true for followedOrganisations in user_schema

--- models/User.ts
+++ models/User.ts
@@ -51,6 +51,7 @@ const UserSchema = new Schema<IUser, UserModel>(
               },
           ],
           default: [],
+          required: true,
       },
     followedFunctions: {
       type: [String],
