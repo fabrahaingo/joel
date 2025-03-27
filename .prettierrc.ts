21bce07c6599f36c7814703ba88cf477ccbc4a33
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2025-03-27 16:07:15 +0100

    fix prettier and ESLINT rule conflict

--- .prettierrc.ts
+++
@@ -1,9 +0,0 @@
-// prettier.config.ts, .prettierrc.ts, prettier.config.mts, or .prettierrc.mts
-
-import { type Config } from "prettier";
-
-const config: Config = {
-  trailingComma: "none"
-};
-
-export default config;
--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -17,5 +18,6 @@ export default tseslint.config(
       }
     }
   },
+  prettierConfig,
   eslintPluginPrettierRecommended
 );
