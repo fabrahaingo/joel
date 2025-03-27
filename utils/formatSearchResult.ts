0577cba54e6d65e0e622ca222a31f95db6bacc41
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2025-03-27 01:06:21 +0100

    improve formatSearchResult formatting with multiple records

--- utils/formatSearchResult.ts
+++ utils/formatSearchResult.ts
@@ -49,7 +49,7 @@ function addPoste(elem: JORFSearchItem, message: string) {
 
 function addLinkJO(elem: JORFSearchItem, message: string) {
   if (elem.source_id && elem.source_date) {
-    message += `🔗 _JO du ${dateToFrenchString(elem.source_date)}_: `;
+    message += `🔗 _${elem.source_name} du ${dateToFrenchString(elem.source_date)}_: `;
 
     switch (elem.source_name) {
       case "BOMI":
@@ -65,27 +65,31 @@ function addLinkJO(elem: JORFSearchItem, message: string) {
 export function formatSearchResult(
   result: JORFSearchItem[],
   options?: {
-    isConfirmation: any;
-    isListing?: any;
-    displayName?: any;
+    isConfirmation?: boolean;
+    isListing?: boolean;
+    displayName?: "all" | "first" | "no";
   },
 ) {
   let message = "";
-  const prenomNom = `${result[0].prenom} ${result[0].nom}`;
-  const prenomNomLink = `[${prenomNom}](https://jorfsearch.steinertriples.ch/name/${encodeURI(
-    prenomNom,
-  )})`;
-  if (options?.isConfirmation) {
-    if (result.length === 1)
-      message += `Voici la dernière information que nous avons sur ${prenomNomLink}.\n\n`;
-    else
-      message += `Voici les ${String(result.length)} dernières informations que nous avons sur ${prenomNomLink}.\n\n`;
-  } else if (!options?.isListing) {
-    message += `Voici la liste des postes connus pour ${prenomNomLink}:\n\n`;
-  }
   for (const elem of result) {
-    if (options?.displayName) {
-      message += `🕵️ *${elem.prenom} ${elem.nom}*\n`;
+    const prenomNom = `${elem.prenom} ${elem.nom}`;
+    const prenomNomLink = `[${prenomNom}](https://jorfsearch.steinertriples.ch/name/${encodeURI(
+        prenomNom,
+    )})`;
+    if (result.indexOf(elem) == 0) {
+      if (options?.isConfirmation) {
+        if (result.length === 1)
+          message += `Voici la dernière information que nous avons sur ${prenomNomLink}.\n\n`;
+        else
+          message += `Voici les ${String(result.length)} dernières informations que nous avons sur ${prenomNomLink}.\n\n`;
+      } else if (!options?.isListing) {
+        message += `Voici la liste des postes connus pour ${prenomNomLink}:\n\n`;
+      } else if (options?.displayName === "first") {
+        message += `🕵️ ${prenomNomLink}\n`;
+      }
+    }
+    if (options?.displayName === "all") {
+      message += `🕵️ ${prenomNomLink}\n`;
     }
     message += textTypeOrdre(elem.type_ordre || "nomination", elem.sexe || "M");
     message = addPoste(elem, message);
