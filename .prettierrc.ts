229cc45bdde41ca3e7e9435795ce08b91c60fefe
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2025-03-27 14:31:19 +0100

    add ESLINT and prettier settings

---
+++ .prettierrc.ts
@@ -0,0 +1,9 @@
+// prettier.config.ts, .prettierrc.ts, prettier.config.mts, or .prettierrc.mts
+
+import { type Config } from "prettier";
+
+const config: Config = {
+  trailingComma: "none",
+};
+
+export default config;
---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
+  },
+  eslintPluginPrettierRecommended
+);
