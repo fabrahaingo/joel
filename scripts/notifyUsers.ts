8c95ac7c72e6136bcc436e5f48c6ce5ed726c98f
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2025-03-28 10:46:09 +0100

    formatting

--- scripts/notifyUsers.ts
+++ scripts/notifyUsers.ts
@@ -12,13 +12,16 @@ import { ChatId } from "node-telegram-bot-api";
 import { Types } from "mongoose";
 import umami from "../utils/umami";
 import { dateTOJORFFormat, JORFtoDate } from "../utils/date.utils";
-import { splitText} from "../utils/sendLongText";
+import { splitText } from "../utils/sendLongText";
 import { formatSearchResult } from "../utils/formatSearchResult";
-import { callJORFSearchDay, uniqueMinimalNameInfo } from "../utils/JORFSearch.utils";
+import {
+  callJORFSearchDay,
+  uniqueMinimalNameInfo
+} from "../utils/JORFSearch.utils";
 import { ObjectId } from "mongodb";
 
 async function getJORFRecordsFromDate(
-    startDate: Date,
+  startDate: Date
 ): Promise<JORFSearchItem[]> {
   const todayDate = new Date();
 
@@ -44,27 +47,35 @@ async function getJORFRecordsFromDate(
   return updatedPeople;
 }
 
-function extractTaggedItems(JORF_items: JORFSearchItem[], tagName: FunctionTags, tagvalue?: string) {
+function extractTaggedItems(
+  JORF_items: JORFSearchItem[],
+  tagName: FunctionTags,
+  tagvalue?: string
+) {
   if (tagvalue === undefined) {
-    return JORF_items.filter((item) =>
-        Object.prototype.hasOwnProperty.call(item, tagName) // Check if item has tag as a field
+    return JORF_items.filter(
+      (item) => Object.prototype.hasOwnProperty.call(item, tagName) // Check if item has tag as a field
     );
   } else {
-    return JORF_items.filter((item) =>
-        Object.prototype.hasOwnProperty.call(item, tagName) // Check if item has tag as a field
-        && item[tagName as keyof JORFSearchItem] === tagvalue // Check if tag has the required value
+    return JORF_items.filter(
+      (item) =>
+        Object.prototype.hasOwnProperty.call(item, tagName) && // Check if item has tag as a field
+        item[tagName as keyof JORFSearchItem] === tagvalue // Check if tag has the required value
     );
   }
 }
 
-export function buildTagMap(updatedRecords: JORFSearchItem[], tagList: FunctionTags[]) {
+export function buildTagMap(
+  updatedRecords: JORFSearchItem[],
+  tagList: FunctionTags[]
+) {
   return tagList.reduce((tagMap: [[FunctionTags], JORFSearchItem[]][], tag) => {
     // extracts the relevant tags from the daily updates
-    const taggedItems=extractTaggedItems(updatedRecords, tag);
+    const taggedItems = extractTaggedItems(updatedRecords, tag);
     if (taggedItems.length == 0) return tagMap; // If no tagged record: we drop the tag
 
     // format: {tag: [contacts], tag2: [contacts]}
-    tagMap[tag as FunctionTags]=taggedItems;
+    tagMap[tag as FunctionTags] = taggedItems;
     return tagMap;
   }, []);
 }
@@ -78,24 +89,42 @@ async function filterOutBlockedUsers(users: IUser[]): Promise<IUser[]> {
 }
 
 // Update the timestamp of last update of a user-specific people follow
-async function updateUserFollowedPeople(user: IUser, updatedPeopleIds: Types.ObjectId[]) {
-  if (updatedPeopleIds.length==0) {
+async function updateUserFollowedPeople(
+  user: IUser,
+  updatedPeopleIds: Types.ObjectId[]
+) {
+  if (updatedPeopleIds.length == 0) {
     return; // So we don't touch the use record
   }
 
   const currentDate = new Date();
 
-  user.followedPeople=user.followedPeople.reduce((followedList: {peopleId: ObjectId, lastUpdate: Date}[], followed)=>{
-    if (followedList.some(f=>f.peopleId.toString()===followed.peopleId.toString())) return followedList; // If user follows twice the same person: we drop the second record
+  user.followedPeople = user.followedPeople.reduce(
+    (followedList: { peopleId: ObjectId; lastUpdate: Date }[], followed) => {
+      if (
+        followedList.some(
+          (f) => f.peopleId.toString() === followed.peopleId.toString()
+        )
+      )
+        return followedList; // If user follows twice the same person: we drop the second record
 
-    // if updated people: we update the timestamp
-    if (updatedPeopleIds.some(p=>p._id.toString()===followed.peopleId.toString())) {
-      followedList.push({ peopleId: followed.peopleId, lastUpdate: currentDate});
-    } else {
-      followedList.push(followed); // otherwise we don't change the item
-    }
-    return followedList;
-  },[])
+      // if updated people: we update the timestamp
+      if (
+        updatedPeopleIds.some(
+          (p) => p._id.toString() === followed.peopleId.toString()
+        )
+      ) {
+        followedList.push({
+          peopleId: followed.peopleId,
+          lastUpdate: currentDate
+        });
+      } else {
+        followedList.push(followed); // otherwise we don't change the item
+      }
+      return followedList;
+    },
+    []
+  );
 
   // save user
   await user.save();
@@ -103,23 +132,29 @@ async function updateUserFollowedPeople(user: IUser, updatedPeopleIds: Types.Obj
 
 // There is currently no way to check if a user has been notified of a tag update
 // Resuming an update thus require to force-notify users for all tags update over the period.
-export async function notifyFunctionTagsUpdates(updatedRecords: JORFSearchItem[]
+export async function notifyFunctionTagsUpdates(
+  updatedRecords: JORFSearchItem[]
 ) {
-
-  const updatedTagMap = buildTagMap(updatedRecords, Object.values(FunctionTags) as FunctionTags[]);
+  const updatedTagMap = buildTagMap(
+    updatedRecords,
+    Object.values(FunctionTags) as FunctionTags[]
+  );
   const updatedTagList = Object.keys(updatedTagMap) as FunctionTags[];
 
-  const usersFollowingTags: IUser[] = await User.find({
-    followedFunctions: {
-      $elemMatch: {
-        $in: updatedTagList,
-      },
+  const usersFollowingTags: IUser[] = await User.find(
+    {
+      followedFunctions: {
+        $elemMatch: {
+          $in: updatedTagList
+        }
+      }
     },
-  }, {
-    _id: 1,
-    chatId: 1,
-    followedFunctions: 1
-  }).then(async (res: IUser[]) => {
+    {
+      _id: 1,
+      chatId: 1,
+      followedFunctions: 1
+    }
+  ).then(async (res: IUser[]) => {
     return await filterOutBlockedUsers(res);
   });
 
@@ -129,67 +164,90 @@ export async function notifyFunctionTagsUpdates(updatedRecords: JORFSearchItem[]
   }
 }
 
-
 export async function notifyPeopleUpdates(updatedRecords: JORFSearchItem[]) {
-
   const updatedPeopleList: IPeople[] = await People.find({
-    $or: uniqueMinimalNameInfo(updatedRecords),
+    $or: uniqueMinimalNameInfo(updatedRecords)
   });
 
   // Fetch all users following at least one of the updated People
-  const updatedUsers: IUser[] = await User.find({
-    followedPeople: {
-      $elemMatch: {
-        peopleId: {
-          $in: updatedPeopleList.map(i=>i._id),
-        },
-      },
+  const updatedUsers: IUser[] = await User.find(
+    {
+      followedPeople: {
+        $elemMatch: {
+          peopleId: {
+            $in: updatedPeopleList.map((i) => i._id)
+          }
+        }
+      }
     },
-  },{
-    _id: 1,
-    chatId: 1,
-    followedPeople: { peopleId: 1, lastUpdate: 1 },
-  }).then(async (res: IUser[]) => {
+    {
+      _id: 1,
+      chatId: 1,
+      followedPeople: { peopleId: 1, lastUpdate: 1 }
+    }
+  ).then(async (res: IUser[]) => {
     return await filterOutBlockedUsers(res); // filter out user who blocked JOEL
   });
 
   for (const user of updatedUsers) {
     // Ids of all people followed by the user
-    const peopleIdsFollowedByUser=user.followedPeople.map(j=>j.peopleId.toString());
-    const peopleFollowedByUser=updatedPeopleList.filter(i=>peopleIdsFollowedByUser.includes(i._id.toString()));
-    const peopleInfoFollowedByUser=uniqueMinimalNameInfo(peopleFollowedByUser)
+    const peopleIdsFollowedByUser = user.followedPeople.map((j) =>
+      j.peopleId.toString()
+    );
+    const peopleFollowedByUser = updatedPeopleList.filter((i) =>
+      peopleIdsFollowedByUser.includes(i._id.toString())
+    );
+    const peopleInfoFollowedByUser =
+      uniqueMinimalNameInfo(peopleFollowedByUser);
 
     // Records which are associated with followed People, and which are new for this respective People follow
-    const newRecordsFollowedByUser=
-        updatedRecords.reduce((recordList: JORFSearchItem[], record) => {
-          // remove records not associated with followed people
-          // this the first main filter
-          if (!peopleInfoFollowedByUser.some(p=> p.nom === record.nom && p.prenom === record.prenom)) return recordList
+    const newRecordsFollowedByUser = updatedRecords.reduce(
+      (recordList: JORFSearchItem[], record) => {
+        // remove records not associated with followed people
+        // this the first main filter
+        if (
+          !peopleInfoFollowedByUser.some(
+            (p) => p.nom === record.nom && p.prenom === record.prenom
+          )
+        )
+          return recordList;
 
-          const updatesPeopleId=peopleFollowedByUser
-              .find(i=>i.nom == record.nom && i.prenom == record.prenom)?._id;
-          if (updatesPeopleId === undefined) return recordList; // this should not happen
+        const updatesPeopleId = peopleFollowedByUser.find(
+          (i) => i.nom == record.nom && i.prenom == record.prenom
+        )?._id;
+        if (updatesPeopleId === undefined) return recordList; // this should not happen
 
-          // Find the follow data associated with this People
-          const followData=user.followedPeople.find(i=> i.peopleId.toString()===updatesPeopleId.toString());
-          if (followData === undefined) return recordList; // this should not happen
+        // Find the follow data associated with this People
+        const followData = user.followedPeople.find(
+          (i) => i.peopleId.toString() === updatesPeopleId.toString()
+        );
+        if (followData === undefined) return recordList; // this should not happen
 
-          // Check that the update is newer than lastUpdate
-          if(JORFtoDate(record.source_date).getTime() < followData.lastUpdate.getTime()) return recordList;
+        // Check that the update is newer than lastUpdate
+        if (
+          JORFtoDate(record.source_date).getTime() <
+          followData.lastUpdate.getTime()
+        )
+          return recordList;
 
-          // Record up to this point are associated to a followed People and newer than the last update
-          recordList.push(record);
-          return recordList
-        }, []);
+        // Record up to this point are associated to a followed People and newer than the last update
+        recordList.push(record);
+        return recordList;
+      },
+      []
+    );
 
     // send notification to user
     await sendPeopleUpdate(user, newRecordsFollowedByUser);
 
-
     // Ids of updated peoples:
-    const updatedRecordsPeopleId=peopleFollowedByUser
-        .filter(p=>newRecordsFollowedByUser.some(r=>( r.nom === p.nom && r.prenom === p.prenom)))
-        .map(p=>p._id);
+    const updatedRecordsPeopleId = peopleFollowedByUser
+      .filter((p) =>
+        newRecordsFollowedByUser.some(
+          (r) => r.nom === p.nom && r.prenom === p.prenom
+        )
+      )
+      .map((p) => p._id);
 
     // update each lastUpdate fields of the user followedPeople
     await updateUserFollowedPeople(user, updatedRecordsPeopleId);
@@ -199,7 +257,7 @@ export async function notifyPeopleUpdates(updatedRecords: JORFSearchItem[]) {
 const BOT_TOKEN = process.env.BOT_TOKEN || "";
 
 async function sendPeopleUpdate(user: IUser, updatedRecords: JORFSearchItem[]) {
-  const nbPersonUpdated=uniqueMinimalNameInfo(updatedRecords).length;
+  const nbPersonUpdated = uniqueMinimalNameInfo(updatedRecords).length;
 
   if (nbPersonUpdated == 0) {
     return;
@@ -211,23 +269,32 @@ async function sendPeopleUpdate(user: IUser, updatedRecords: JORFSearchItem[]) {
   const pluralHandler = updatedRecords.length > 1 ? "s" : "";
 
   let notification_text = `📢 Nouvelle${pluralHandler} publication${pluralHandler} parmi les personnes que vous suivez :\n\n`;
-  notification_text += formatSearchResult(updatedRecords, { isConfirmation: false, isListing: true, displayName: "all" });
+  notification_text += formatSearchResult(updatedRecords, {
+    isConfirmation: false,
+    isListing: true,
+    displayName: "all"
+  });
 
   await sendLongMessageFromAxios(user, notification_text);
 
   await umami.log({ event: "/notification-update-people" });
 }
 
-async function sendTagUpdates(user: IUser, tagMap: [[FunctionTags], JORFSearchItem[]][]) {
-
+async function sendTagUpdates(
+  user: IUser,
+  tagMap: [[FunctionTags], JORFSearchItem[]][]
+) {
   // only keep the tags followed by the user
-  const tagList = (Object.keys(tagMap) as FunctionTags[]).filter(tag=>user.followedFunctions.includes(tag));
+  const tagList = (Object.keys(tagMap) as FunctionTags[]).filter((tag) =>
+    user.followedFunctions.includes(tag)
+  );
 
   if (tagList.length == 0) {
     return;
   }
 
-  let notification_text = "📢 Nouvelles publications parmi les fonctions que suivez :\n\n";
+  let notification_text =
+    "📢 Nouvelles publications parmi les fonctions que suivez :\n\n";
 
   // We preload the tag keys and values to reduce search time
   const tagValues = Object.values(FunctionTags);
@@ -243,7 +310,11 @@ async function sendTagUpdates(user: IUser, tagMap: [[FunctionTags], JORFSearchIt
     const pluralHandler = tagRecords.length > 1 ? "s" : "";
     notification_text += `Nouvelle${pluralHandler} publication${pluralHandler} pour la fonction *${tagKey}*\n\n`;
 
-    notification_text += formatSearchResult(tagRecords, { isConfirmation: false, isListing: true, displayName: "all" });
+    notification_text += formatSearchResult(tagRecords, {
+      isConfirmation: false,
+      isListing: true,
+      displayName: "all"
+    });
 
     if (tagList.indexOf(tagValue) + 1 !== tagList.length)
       notification_text += "====================\n\n";
@@ -261,34 +332,34 @@ async function sendLongMessageFromAxios(user: IUser, message: string) {
 
   for (const message of messagesArray) {
     await axios
-        .post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
-          chat_id: user.chatId as ChatId,
-          text: message,
-          parse_mode: "markdown",
-          link_preview_options: {
-            is_disabled: true,
-          },
-        })
-        .catch(async (err: unknown) => {
-          const error = err as AxiosError;
-          if (error.response !== undefined &&
-              (error.response.data.description as string) ===
-              "Forbidden: bot was blocked by the user"
-          ) {
-            await umami.log({ event: "/user-blocked-joel" });
-            await new Blocked({
-              chatId: user.chatId as ChatId,
-            }).save();
-            return;
-          }
-          console.log(error.message);
-        });
+      .post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+        chat_id: user.chatId as ChatId,
+        text: message,
+        parse_mode: "markdown",
+        link_preview_options: {
+          is_disabled: true
+        }
+      })
+      .catch(async (err: unknown) => {
+        const error = err as AxiosError;
+        if (
+          error.response !== undefined &&
+          (error.response.data.description as string) ===
+            "Forbidden: bot was blocked by the user"
+        ) {
+          await umami.log({ event: "/user-blocked-joel" });
+          await new Blocked({
+            chatId: user.chatId as ChatId
+          }).save();
+          return;
+        }
+        console.log(error.message);
+      });
 
     // prevent hitting Telegram API rate limit
     await new Promise((resolve) => setTimeout(resolve, 100));
   }
 }
-
 
 if (BOT_TOKEN === undefined) {
   throw new Error("BOT TOKEN NOT SET");
@@ -315,15 +386,18 @@ if (MONGODB_URI === undefined) {
   // currentDate is today
   const currentDate = new Date();
   const startDate = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth(),
-      currentDate.getDate() - shiftDays,
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    currentDate.getDate() - shiftDays
   );
   startDate.setHours(0, 0, 0, 0);
   // Fetch all records from the start date
   const JORFAllRecordsFromDate = await getJORFRecordsFromDate(startDate);
   // Sort records by date
-  JORFAllRecordsFromDate.sort((i,j)=>JORFtoDate(i.source_date).getTime()-JORFtoDate(j.source_date).getTime());
+  JORFAllRecordsFromDate.sort(
+    (i, j) =>
+      JORFtoDate(i.source_date).getTime() - JORFtoDate(j.source_date).getTime()
+  );
 
   // Send notifications to users on followed people
   await notifyPeopleUpdates(JORFAllRecordsFromDate);
--- utils/JORFSearch.utils.ts
+++ utils/JORFSearch.utils.ts
@@ -1,118 +1,139 @@
-import { cleanJORFItems, JORFSearchResponse } from "../entities/JORFSearchResponse";
+import {
+  cleanJORFItems,
+  JORFSearchResponse
+} from "../entities/JORFSearchResponse";
 import { WikiDataId } from "../types";
 import axios from "axios";
 
 export async function callJORFSearchPeople(peopleName: string) {
-    try {
-        return axios
-            .get<JORFSearchResponse>(encodeURI(
-                `https://jorfsearch.steinertriples.ch/name/${
-                    cleanPeopleName(peopleName) // Cleaning the string reduces the number of calls to JORFSearch
-                }?format=JSON`
-            )
-        ).then(async (res1) => {
-            if (res1.data === null ) return []; // If an error occurred
-            if (typeof res1.data !== "string") return res1.data; // If it worked
+  try {
+    return axios
+      .get<JORFSearchResponse>(
+        encodeURI(
+          `https://jorfsearch.steinertriples.ch/name/${
+            cleanPeopleName(peopleName) // Cleaning the string reduces the number of calls to JORFSearch
+          }?format=JSON`
+        )
+      )
+      .then(async (res1) => {
+        if (res1.data === null) return []; // If an error occurred
+        if (typeof res1.data !== "string") return res1.data; // If it worked
 
-            // If the peopleName had nom/prenom inverted or bad formatting:
-            // we need to call JORFSearch again with the response url with correct format
-            if (res1.request.res.responseUrl) {
-                return await axios
-                    .get<JORFSearchResponse>(
-                    res1.request.res.responseUrl.endsWith("?format=JSON")
-                        ? res1.request.res.responseUrl
-                        : `${res1.request.res.responseUrl}?format=JSON`
-                ).then(async (res2) => {
-                        if (res2.data === null || typeof res2.data === "string") {
-                            return [];
-                        }
-                        return res2.data;
-                    });
-            }
-            return []
-        }).then(res=> cleanJORFItems(res));
-    } catch (error) {
-        console.log(error);
+        // If the peopleName had nom/prenom inverted or bad formatting:
+        // we need to call JORFSearch again with the response url with correct format
+        if (res1.request.res.responseUrl) {
+          return await axios
+            .get<JORFSearchResponse>(
+              res1.request.res.responseUrl.endsWith("?format=JSON")
+                ? res1.request.res.responseUrl
+                : `${res1.request.res.responseUrl}?format=JSON`
+            )
+            .then(async (res2) => {
+              if (res2.data === null || typeof res2.data === "string") {
+                return [];
+              }
+              return res2.data;
+            });
+        }
         return [];
-    }
+      })
+      .then((res) => cleanJORFItems(res));
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
 
-export async function callJORFSearchDay(day: Date){
-    try {
-        return axios
-        .get<JORFSearchResponse>(encodeURI(
-            `https://jorfsearch.steinertriples.ch/${
+export async function callJORFSearchDay(day: Date) {
+  try {
+    return axios
+      .get<JORFSearchResponse>(
+        encodeURI(
+          `https://jorfsearch.steinertriples.ch/${
             day.toLocaleDateString("fr-FR").split("/").join("-") // format day = "18-02-2024";
-        }?format=JSON`))
-        .then((res) => {
-            if (res.data === null || typeof res.data === "string") return [];
-            return cleanJORFItems(res.data);
-        });
-    } catch (error) {
-        console.log(error);
-    }
-    return [];
+          }?format=JSON`
+        )
+      )
+      .then((res) => {
+        if (res.data === null || typeof res.data === "string") return [];
+        return cleanJORFItems(res.data);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+  return [];
 }
 
 export async function callJORFSearchTag(tag: string, tagValue?: string) {
-    try {
-        return axios
-            .get<JORFSearchResponse>(encodeURI(
-                `https://jorfsearch.steinertriples.ch/tag/${tag}${
-                    tagValue !== undefined ? `="${tagValue}"` : ``
-                }?format=JSON`))
-            .then((res) => {
-                if (res.data === null || typeof res.data === "string") return [];
-                return cleanJORFItems(res.data);
-            });
-    } catch (error) {
-        console.log(error);
-    }
-    return [];
+  try {
+    return axios
+      .get<JORFSearchResponse>(
+        encodeURI(
+          `https://jorfsearch.steinertriples.ch/tag/${tag}${
+            tagValue !== undefined ? `="${tagValue}"` : ``
+          }?format=JSON`
+        )
+      )
+      .then((res) => {
+        if (res.data === null || typeof res.data === "string") return [];
+        return cleanJORFItems(res.data);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+  return [];
 }
 
 export async function callJORFSearchOrganisation(wikiId: WikiDataId) {
-    try {
-        return axios
-        .get<JORFSearchResponse>(encodeURI(
-            `https://jorfsearch.steinertriples.ch/${wikiId}?format=JSON`))
-        .then((res) => {
-            if (res.data === null || typeof res.data === "string") return [];
-            return cleanJORFItems(res.data);
-        });
-    } catch (error) {
-        console.log(error);
-    }
-    return [];
+  try {
+    return axios
+      .get<JORFSearchResponse>(
+        encodeURI(`https://jorfsearch.steinertriples.ch/${wikiId}?format=JSON`)
+      )
+      .then((res) => {
+        if (res.data === null || typeof res.data === "string") return [];
+        return cleanJORFItems(res.data);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+  return [];
 }
 
 // Format a string to match the expected search format on JORFSearch: first letter capitalized and no accent
 function cleanPeopleName(input: string): string {
-    /// To lower case
-    input = input.trim().toLowerCase();
+  /// To lower case
+  input = input.trim().toLowerCase();
 
-    // Replace non-standard URL characters
-    input = input.replace(/[àáâãäå]/g, "a");
-    input = input.replace(/[èéêë]/g, "e");
-    input = input.replace(/[ìíîï]/g, "i");
-    input = input.replace(/[òóôõö]/g, "o");
-    input = input.replace(/[ùúûü]/g, "u");
-    input = input.replace(/[ç]/g, "c");
+  // Replace non-standard URL characters
+  input = input.replace(/[àáâãäå]/g, "a");
+  input = input.replace(/[èéêë]/g, "e");
+  input = input.replace(/[ìíîï]/g, "i");
+  input = input.replace(/[òóôõö]/g, "o");
+  input = input.replace(/[ùúûü]/g, "u");
+  input = input.replace(/[ç]/g, "c");
 
-    // Capitalized the first letter of each part of the name
-    input=input.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+  // Capitalized the first letter of each part of the name
+  input = input
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 
-    return input;
+  return input;
 }
 
 interface NameInfo {
-    nom: string,
-    prenom: string
+  nom: string;
+  prenom: string;
 }
 export function uniqueMinimalNameInfo(records: NameInfo[]) {
-    return records.reduce((infoList: { nom: string, prenom: string}[], item)=>{
-        if (infoList.find(i=> i.nom === item.nom && i.prenom == item.prenom) !== undefined) return infoList
-        infoList.push({nom: item.nom, prenom: item.prenom})
-        return infoList;
-    },[]);
+  return records.reduce((infoList: { nom: string; prenom: string }[], item) => {
+    if (
+      infoList.find((i) => i.nom === item.nom && i.prenom == item.prenom) !==
+      undefined
+    )
+      return infoList;
+    infoList.push({ nom: item.nom, prenom: item.prenom });
+    return infoList;
+  }, []);
 }
