d5d860e4a19ebd34737982fece0a575df8a0b459
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2025-07-07 20:16:54 +0200

    moved command to function mapping to a separate file

--- app.ts
+++ app.ts
@@ -1,83 +1,14 @@
 import "dotenv/config";
 import TelegramBot from "node-telegram-bot-api";
-import { CommandType } from "./types.js";
 import { mongodbConnect } from "./db.js";
 import { TelegramSession } from "./entities/TelegramSession.js";
-
-import { followOrganisationCommand } from "./commands/followOrganisation.js";
-import { followCommand, fullHistoryCommand, searchCommand } from "./commands/search.js";
-import { enaCommand, promosCommand } from "./commands/ena.js";
-import { statsCommand } from "./commands/stats.js";
-import { defaultCommand } from "./commands/default.js";
-import { startCommand } from "./commands/start.js";
-import { deleteProfileCommand } from "./commands/deleteProfile.js";
-import { helpCommand } from "./commands/help.js";
-import { followFunctionCommand } from "./commands/followFunction.js";
-import { listCommand, unfollowCommand } from "./commands/list.js";
+import { commands } from "./commands/Commands.js";
 
 const bot: TelegramBot = new TelegramBot(process.env.BOT_TOKEN ?? "", {
   polling: true,
   onlyFirstMatch: true,
 });
 
-const commands: CommandType[] = [
-  {
-    regex: /\/start$|🏠 Menu principal/,
-    action: startCommand,
-  },
-  {
-    regex: /🔎 Rechercher$|🔎 Nouvelle recherche$/,
-    action: searchCommand,
-  },
-  {
-    regex: /Historique de \s*(.*)/i,
-    action: fullHistoryCommand,
-  },
-  {
-    regex: /Suivre \s*(.*)/i,
-    action: followCommand,
-  },
-  {
-    regex: /✋ Retirer un suivi$/,
-    action: unfollowCommand,
-  },
-  {
-    regex: /🧐 Lister mes suivis$/,
-    action: listCommand,
-  },
-  {
-    regex: /❓ Aide/,
-    action: helpCommand,
-  },
-  {
-    regex: /👨‍💼 Ajouter une fonction/,
-    action: followFunctionCommand,
-  },
-  {
-    regex: /\/secret|\/ENA|\/INSP/i,
-    action: enaCommand,
-  },
-  {
-    regex: /\/promos/,
-    action: promosCommand,
-  },
-  {
-    regex: /\/stats/,
-    action: statsCommand,
-  },
-  {
-    regex: /🏛️️ Ajouter une organisation|\/followOrganisation|\/followOrganization/i,
-    action: followOrganisationCommand,
-  },
-  {
-    regex: /\/supprimerCompte/,
-    action: deleteProfileCommand,
-  },
-  {
-    regex: /.*/,
-    action: defaultCommand,
-  },
-];
 
 await (async () => {
   await mongodbConnect();
---
+++ commands/Commands.ts
@@ -0,0 +1,71 @@
+import { CommandType } from "../types.js";
+
+import { followOrganisationCommand } from "./followOrganisation.js";
+import { followCommand, fullHistoryCommand, searchCommand } from "./search.js";
+import { enaCommand, promosCommand } from "./ena.js";
+import { statsCommand } from "./stats.js";
+import { defaultCommand } from "./default.js";
+import { startCommand } from "./start.js";
+import { deleteProfileCommand } from "./deleteProfile.js";
+import { helpCommand } from "./help.js";
+import { followFunctionCommand } from "./followFunction.js";
+import { listCommand, unfollowCommand } from "./list.js";
+
+export const commands: CommandType[] = [
+    {
+        regex: /\/start$|🏠 Menu principal/,
+        action: startCommand,
+    },
+    {
+        regex: /🔎 Rechercher$|🔎 Nouvelle recherche$/,
+        action: searchCommand,
+    },
+    {
+        regex: /Historique de \s*(.*)/i,
+        action: fullHistoryCommand,
+    },
+    {
+        regex: /Suivre \s*(.*)/i,
+        action: followCommand,
+    },
+    {
+        regex: /✋ Retirer un suivi$/,
+        action: unfollowCommand,
+    },
+    {
+        regex: /🧐 Lister mes suivis$/,
+        action: listCommand,
+    },
+    {
+        regex: /❓ Aide/,
+        action: helpCommand,
+    },
+    {
+        regex: /👨‍💼 Ajouter une fonction/,
+        action: followFunctionCommand,
+    },
+    {
+        regex: /\/secret|\/ENA|\/INSP/i,
+        action: enaCommand,
+    },
+    {
+        regex: /\/promos/,
+        action: promosCommand,
+    },
+    {
+        regex: /\/stats/,
+        action: statsCommand,
+    },
+    {
+        regex: /🏛️️ Ajouter une organisation|\/followOrganisation|\/followOrganization/i,
+        action: followOrganisationCommand,
+    },
+    {
+        regex: /\/supprimerCompte/,
+        action: deleteProfileCommand,
+    },
+    {
+        regex: /.*/,
+        action: defaultCommand,
+    },
+];
