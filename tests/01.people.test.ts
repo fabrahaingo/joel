b7aab114499044ca892a8c809ed492d01cfb2b83
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2025-07-14 14:56:10 +0200

    implement legacy People conversion test

--- tests/01.people.test.ts
+++ tests/01.people.test.ts
@@ -3,65 +3,65 @@ import People, { LegacyPeople_V1 } from "../models/People.ts";
 import { IPeople } from "../types.ts";
 import { JORFSearchItem } from "../entities/JORFSearchResponse.ts";
 import mongoose from "mongoose";
-import { MongoMemoryServer } from "mongodb-memory-server";
 
 describe("People Model Test Suite", () => {
   let mdb: typeof mongoose.connection.db;
-  let mms: MongoMemoryServer;
 
-  beforeAll(async () => {
-    mms = await MongoMemoryServer.create();
-    await mongoose.connect(mms.getUri(), {
-      dbName: "jest"
-    });
+  beforeAll(() => {
+    mdb = mongoose.connection.db;
   });
 
-  afterEach(async () => {
-    if (!mdb) throw new Error("MongoDB connection not established");
-    await mdb.dropDatabase();
+  beforeEach(async () => {
+    if (!mongoose.connection.db)
+      throw new Error("MongoDB connection not established");
+    await mongoose.connection.db.dropDatabase();
   });
 
-  afterAll(async () => {
-    await mongoose.disconnect();
-    await mms.stop();
-  });
+  const legacyPersonData = {
+    //_id: Types.ObjectId: not here as the record is inserted
+    nom: "Macron",
+    prenom: "Emmanuel",
+    lastKnownPosition: {
+      organisations: [
+        {
+          nom: "École nationale d'administration (ENA)",
+          wikidata_id: "Q273579",
+          etablissement_enseignement_superieur: "Q38723"
+        }
+      ],
+      source_date: "1993-03-24",
+      source_id: "JORFTEXT000000345548",
+      source_name: "JORF",
+      type_ordre: "nomination",
+      date_debut: "1993-01-01",
+      ecole: "true",
+      eleve_ena: "1993-1995",
+      sexe: "F",
+      nom: "Macron",
+      prenom: "Emmanuel"
+    },
+    createAt: Date.now(),
+    updatedAt: Date.now()
+  };
+
+  const currentPersonData = {
+    nom: "Macron",
+    prenom: "Emmanuel"
+  };
 
   // Only need to be checked for the current user schema
   describe("Schema Validation", () => {
-    const currentPersonData = {
-      //_id: Types.ObjectId: not here as the record is inserted
-      nom: "Macron",
-      prenom: "Emmanuel",
-      lastKnownPosition: {
-        organisations: [
-          {
-            nom: "École nationale d'administration (ENA)",
-            wikidata_id: "Q273579",
-            etablissement_enseignement_superieur: "Q38723"
-          }
-        ],
-        source_date: "1993-03-24",
-        source_id: "JORFTEXT000000345548",
-        source_name: "JORF",
-        type_ordre: "nomination",
-        date_debut: "1993-01-01",
-        ecole: "true",
-        eleve_ena: "1993-1995",
-        sexe: "F",
-        nom: "Macron",
-        prenom: "Emmanuel"
-      }
-    };
-
-    it("should insert a legacy people in the db and check if t", async () => {
+    it("should insert a legacy People in the db and validate it use", async () => {
       if (!mdb) throw new Error("MongoDB connection not established");
-      await mdb.collection("people").insertOne(currentPersonData);
-      const legacyPeople: IPeople | null = (await mdb
-        .collection("people")
-        .findOne({ _id: currentPersonData })) as IPeople | null;
+
+      await People.collection.insertOne(legacyPersonData);
+      const legacyPeople: IPeople | null = await People.findOne({
+        nom: currentPersonData.nom,
+        prenom: currentPersonData.prenom
+      });
       expect(legacyPeople).not.toBeNull();
       if (legacyPeople != null) {
-        expect(await legacyPeople.validate()).toBe(true);
+        await expect(legacyPeople.validate()).resolves.toBeUndefined(); // undefined = validation passed
         expect(
           (legacyPeople as unknown as LegacyPeople_V1).lastKnownPosition
         ).toBeUndefined();
@@ -69,6 +69,7 @@ describe("People Model Test Suite", () => {
     });
 
     it("should find and remove a person from its name", async () => {
+      await People.create(currentPersonData);
       const people: IPeople | null = await People.findOne({
         nom: currentPersonData.nom,
         prenom: currentPersonData.prenom
@@ -79,7 +80,7 @@ describe("People Model Test Suite", () => {
         nom: currentPersonData.nom,
         prenom: currentPersonData.prenom
       });
-      expect(await People.countDocuments()).toBe(1); // Legacy user from 01.legacyUser.test
+      expect(await People.countDocuments()).toBe(0);
     });
 
     it("should fail without required nom", async () => {
@@ -87,8 +88,9 @@ describe("People Model Test Suite", () => {
         prenom: currentPersonData.prenom
       };
 
-      const person: IPeople = People.create(invalidPerson);
-      await expect(person.save()).rejects.toThrow(/nom.*required/);
+      await expect(People.create(invalidPerson)).rejects.toThrow(
+        /nom.*required/
+      );
     });
 
     it("should fail without required prenom", async () => {
@@ -96,8 +98,9 @@ describe("People Model Test Suite", () => {
         nom: currentPersonData.nom
       };
 
-      const person: IPeople = People.create(invalidPerson);
-      await expect(person.save()).rejects.toThrow(/prenom.*required/);
+      await expect(People.create(invalidPerson)).rejects.toThrow(
+        /prenom.*required/
+      );
     });
   });
 
@@ -113,97 +116,74 @@ describe("People Model Test Suite", () => {
 
   describe("Static Methods", () => {
     describe("firstOrCreate", () => {
-      describe(`Current People creation with firstOrCreate`, () => {
-        it("should create a new person if not exists", async () => {
-          const newPerson = {
-            nom: "Dupont",
-            prenom: "Jean",
-            lastKnownPosition: mockJORFItem
-          };
+      it("should create a new person if not exists", async () => {
+        const newPerson = {
+          nom: "Dupont",
+          prenom: "Jean",
+          lastKnownPosition: mockJORFItem
+        };
 
-          const person: IPeople = await People.firstOrCreate(newPerson);
+        const person: IPeople = await People.firstOrCreate(newPerson);
 
-          expect(person.nom).toBe(newPerson.nom);
-          expect(person.prenom).toBe(newPerson.prenom);
+        expect(person.nom).toBe(newPerson.nom);
+        expect(person.prenom).toBe(newPerson.prenom);
 
-          expect(await People.countDocuments()).toBe(2); // Legacy user from 01.legacyUser.test + currentUser
+        expect(await People.countDocuments()).toBe(1);
+      });
+
+      it("should find person case-insensitively", async () => {
+        const existingPerson: IPeople = await People.create(currentPersonData);
+
+        const personCAPS = await People.firstOrCreate({
+          nom: currentPersonData.nom.toUpperCase(),
+          prenom: currentPersonData.prenom.toUpperCase()
         });
+
+        expect(await People.countDocuments()).toBe(1); // No new records
+
+        expect(personCAPS._id).toEqual(existingPerson._id);
+        expect(personCAPS.nom).toBe(existingPerson.nom);
+        expect(personCAPS.prenom).toBe(existingPerson.prenom);
       });
     });
 
-    ["Legacy", "Current"].forEach((person_gen, user_id) => {
-      let listPersonsByDate: IPeople[] = [];
-      let existingPerson: IPeople;
-
-      describe(`${person_gen} People firstOrCreate`, () => {
-        beforeEach(async () => {
-          if (listPersonsByDate.length === 0) {
-            listPersonsByDate = (await People.find({})).sort(
-              (a, b) => a.createdAt.getTime() - b.createdAt.getTime()
-            );
+    describe("Timestamps", () => {
+      it("should set createdAt and updatedAt on creation", async () => {
+        const person: IPeople = await People.create({
+          nom: "Dupont",
+          prenom: "Jean",
+          lastKnownPosition: {
+            source_date: "2023-01-01"
           }
-          existingPerson = listPersonsByDate[user_id];
         });
 
-        it("should find person case-insensitively", async () => {
-          expect(existingPerson).toBeDefined();
-
-          const peopleCountBefore = await People.countDocuments();
-
-          await People.create(existingPerson).save();
-
-          const personCAPS = await People.firstOrCreate({
-            nom: existingPerson.nom.toUpperCase(),
-            prenom: existingPerson.prenom.toUpperCase()
-          });
-
-          const peopleCountAfter = await People.countDocuments();
-          expect(peopleCountAfter).toBe(peopleCountBefore);
-
-          expect(personCAPS._id).toEqual(existingPerson._id);
-          expect(personCAPS.nom).toBe(existingPerson.nom);
-          expect(personCAPS.prenom).toBe(existingPerson.prenom);
-        });
+        expect(person.createdAt).toBeInstanceOf(Date);
+        expect(person.updatedAt).toBeInstanceOf(Date);
+        expect(person.createdAt).toEqual(person.updatedAt);
       });
-    });
-  });
 
-  describe("Timestamps", () => {
-    it("should set createdAt and updatedAt on creation", async () => {
-      const person: IPeople = await People.create({
-        nom: "Dupont",
-        prenom: "Jean",
-        lastKnownPosition: {
-          source_date: "2023-01-01"
-        }
-      }).save();
+      it("should update only updatedAt on modification", async () => {
+        const person: IPeople = await People.create({
+          nom: "Dupont",
+          prenom: "Jean",
+          lastKnownPosition: {
+            source_date: "2023-01-01"
+          }
+        });
 
-      expect(person.createdAt).toBeInstanceOf(Date);
-      expect(person.updatedAt).toBeInstanceOf(Date);
-      expect(person.createdAt).toEqual(person.updatedAt);
-    });
+        const createdAt = person.createdAt;
 
-    it("should update only updatedAt on modification", async () => {
-      const person: IPeople = await People.create({
-        nom: "Dupont",
-        prenom: "Jean",
-        lastKnownPosition: {
-          source_date: "2023-01-01"
-        }
-      }).save();
+        // Wait a bit to ensure different timestamp
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      const createdAt = person.createdAt;
+        person.nom = "Durant";
+        await person.save();
 
-      // Wait a bit to ensure different timestamp
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      person.nom = "Durant";
-      await person.save();
-
-      expect(person.createdAt).toEqual(createdAt);
-      expect(person.updatedAt.getTime()).toBeGreaterThan(
-        person.createdAt.getTime()
-      );
+        expect(person.createdAt).toEqual(createdAt);
+        expect(person.updatedAt.getTime()).toBeGreaterThan(
+          person.createdAt.getTime()
+        );
+      });
     });
   });
 });
