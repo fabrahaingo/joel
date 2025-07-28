9d55eb96e1100fa0c98bcfe32a3bc9e8979fc2c3
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2025-07-28 22:30:52 +0200

    extend tsconfig.json, switch to ESNext

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
   globalSetup: "<rootDir>/tests/globalSetup.ts",
   globalTeardown: "<rootDir>/tests/globalTeardown.ts",
   setupFilesAfterEnv: ["<rootDir>/tests/setupFile.ts"],
@@ -12,7 +13,7 @@ export default {
     "**/**/*.{ts,tsx}",
     "!src/**/*.d.ts"
   ],
-  coverageProvider: "v8", // or “babel”
+  coverageProvider: "v8",
   coverageDirectory: "coverage",
   coverageReporters: ["json-summary"]
 };
