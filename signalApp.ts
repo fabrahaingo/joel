72b85c02cdbc8c57fa1870239528aa9c14fbfbc0
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2025-08-02 02:46:11 +0200

    create an interface for the SignalMessage object

--- signalApp.ts
+++ signalApp.ts
@@ -20,6 +20,15 @@ if (SIGNAL_BAT_PATH === undefined) {
   throw new Error("SIGNAL_BAT_PATH env variable not set");
 }
 
+interface ISignalMessage {
+  envelope: {
+    sourceNumber: string;
+    dataMessage?: {
+      message?: string;
+    };
+    receiptMessage?: never;
+  };
+}
 await (async () => {
   try {
     await mongodbConnect();
@@ -31,13 +40,12 @@ await (async () => {
     await signal.connect();
 
     // Listen for incoming messages
-    signal.on("message", (message) => {
+    signal.on("message", (message: ISignalMessage) => {
       void (async () => {
-        if ("receiptMessage" in message.envelope) return;
+        const msgText = message.envelope.dataMessage?.message;
+        if (msgText === undefined) return;
 
         await umami.log({ event: "/message-signal" });
-
-        //console.log("Received message:", message.envelope.dataMessage.message);
 
         const signalSession = new SignalSession(
           signal,
@@ -50,8 +58,6 @@ await (async () => {
         if (signalSession.user != null)
           await signalSession.user.updateInteractionMetrics();
 
-        const msgText = message.envelope.dataMessage.message;
-
         for (const command of commands) {
           if (command.regex.test(msgText)) {
             await command.action(signalSession, msgText);
@@ -61,10 +67,10 @@ await (async () => {
       })();
     });
 
+    console.log(`\u{2705} JOEL started successfully`);
+
     // Graceful shutdown
     //await signal.gracefulShutdown();
-
-    console.log(`\u{2705} JOEL started successfully`);
   } catch (error) {
     console.log(error);
   }
