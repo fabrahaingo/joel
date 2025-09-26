b35550b763c41b071c67e44fb50b3b2b17e92d4d
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2025-09-26 16:32:13 +0200

    update help and start messages

--- commands/start.ts
+++ commands/start.ts
@@ -1,6 +1,7 @@
 import { ISession } from "../types.ts";
 import { BotMessages } from "../entities/BotMessages.ts";
 import { processMessage } from "./Commands.ts";
+import { getHelpText } from "./help.ts";
 
 export const startCommand = async (
   session: ISession,
@@ -11,22 +12,9 @@ export const startCommand = async (
   try {
     await session.sendTypingAction();
 
-    const botName = process.env.BOT_NAME;
-
-    let message = BotMessages.START.replace(
-      "{botName}",
-      botName ?? "undefined"
-    );
-
-    message = message.replace(
-      "{LINK_PRIVACY_POLICY}",
-      `[Politique de confidentialité](${BotMessages.URL_PRIVACY_POLICY})`
-    );
-    message = message.replace(
-      "{LINK_GCU}",
-      `[Conditions générales d'utilisation](${BotMessages.URL_GCU})`
-    );
-    await session.sendMessage(message);
+    await session.sendMessage(getHelpText(session), {
+      separateMenuMessage: true
+    });
 
     // if "Bonjour JOEL ! Suivre ..." or "/start Suivre ..."
     if (messageSplit.length > 1 && messageSplit[1] !== "") {
