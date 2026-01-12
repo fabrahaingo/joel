02a470a0b9abf96a32ed3d5e36619f3957335926
Dany Mestas <94061157+DanyPM@users.noreply.github.com>
2026-01-12 15:16:19 +0100

    fix mongoose states imports

--- db.ts
+++ db.ts
@@ -1,4 +1,4 @@
-import mongoose, { ConnectionStates } from "mongoose";
+import mongoose from "mongoose";
 import { ErrorMessages } from "./entities/ErrorMessages.ts";
 
 export const mongodbConnect = async () => {
@@ -6,18 +6,19 @@ export const mongodbConnect = async () => {
     throw new Error(ErrorMessages.MONGODB_URI_NOT_SET);
   }
   if (
-    mongoose.connection.readyState === ConnectionStates.connected ||
-    mongoose.connection.readyState === ConnectionStates.connecting
+    mongoose.connection.readyState === mongoose.ConnectionStates.connected ||
+    mongoose.connection.readyState === mongoose.ConnectionStates.connecting
   )
     return;
+
   await mongoose.connect(process.env.MONGODB_URI);
 };
 
 export const mongodbDisconnect = async () => {
   // Mongoose state values: 0=disconnected, 1=connected, 2=connecting, 3=disconnecting
   if (
-    mongoose.connection.readyState === ConnectionStates.disconnected ||
-    mongoose.connection.readyState === ConnectionStates.disconnecting
+    mongoose.connection.readyState === mongoose.ConnectionStates.disconnected ||
+    mongoose.connection.readyState === mongoose.ConnectionStates.disconnecting
   )
     return;
 
